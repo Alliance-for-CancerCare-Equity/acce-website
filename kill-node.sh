f650774ea56858f6698693fb6a56c74a745a9cf6
Roger Chen <rogerchen803@gmail.com>
2025-08-01 08:43:10 -0700

    [fix] dependancy issues with cms

--- kill-node.sh
+++ kill-node.sh
@@ -9,12 +9,7 @@ if [ -n "$PID" ]; then
   # Get the command name of the process
   CMD=$(ps -p $PID -o comm=)
 
-  # if [[ "$CMD" == "node" ]]; then
-  #     echo "Killing Node.js process ($PID) on port $PORT"
   kill -9 $PID
-  # else
-  #     echo "Port $PORT is in use by '$CMD', not killing."
-  # fi
 else
   echo "No process found on port $PORT"
 fi
